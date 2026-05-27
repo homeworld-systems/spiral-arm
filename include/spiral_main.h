d13b90f396e4b1b9a635a2d660dddab05a0a5856
noelle <nbunny2010@gmail.com>
2026-05-27 17:40:49 -0400

    sometimes we forget about headers and that's okay

--- include/spiral_main.h
+++ include/spiral_main.h
@@ -8,6 +8,8 @@ extern SPIRAL_Window* SPIRAL_main_window;
 
 void SPIRAL_Initialize (const char* window_name);
 
+void SPIRAL_MainMenu (void (*main_menu_init)(), void (*main_menu_loop)());
+
 void SPIRAL_RunChapter (void (*chapter_init)(), void (*chapter_loop)());
 
 void SPIRAL_ExitChapter ();
